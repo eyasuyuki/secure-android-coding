1dc6ed88c37cab47374cd9e8703bbd973146478a
eyasuyuki@gmail.com <eyasuyuki@gmail.com@99484277-7f74-ce71-cf82-0c6185048e4c>
2011-09-27 22:48:21 +0000

    add

---
+++ secure-android-basic-server/src/com/example/basic/Secure_android_basic_serverServlet.java
@@ -0,0 +1,11 @@
+package com.example.basic;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Secure_android_basic_serverServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
