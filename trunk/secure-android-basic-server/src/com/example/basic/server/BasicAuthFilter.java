3d1ccd374e324c393ace54c8f5f7c10e5f3c9957
eyasuyuki@gmail.com <eyasuyuki@gmail.com@99484277-7f74-ce71-cf82-0c6185048e4c>
2011-10-01 06:40:46 +0000

    match sha-256 digested password.

--- trunk/secure-android-basic-server/src/com/example/basic/server/BasicAuthFilter.java
+++ trunk/secure-android-basic-server/src/com/example/basic/server/BasicAuthFilter.java
@@ -1,9 +1,25 @@
+/*
+ * 下記を参考にしました:
+ * 
+ * 中田 秀基「すっきりわかるGoogle App Engine for Javaクラウドプログラミング」
+ * ISBN978-4-7973-5760-8
+ * p.146-149
+ * 
+ * 徳丸 浩「体系的に学ぶ 安全なWebアプリケーションの作り方」
+ * ISBN978-4-7973-6119-3
+ * p.327-328
+ */
+
 package com.example.basic.server;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -18,39 +34,57 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang.ArrayUtils;
 
 public class BasicAuthFilter implements Filter {
 	private Logger logger = Logger.getLogger(BasicAuthFilter.class.getName());
 	
+	private static final byte[] FIXED_SALT = "aec0dbc8e6348d590d8f8cea55b573b4602a4150".getBytes();
+	private static final int STRETCH_COUNT = 1;
+	
 	public static final String BASIC_AUTH_USERNAME = "BASIC_AUTH_USERNAME";
 	
 	Map<String, String> userMap = new HashMap<String, String>();
+
+	static byte[] getSalt(String str) {
+		byte[] src = str.getBytes();
+		return ArrayUtils.addAll(src, FIXED_SALT);
+	}
+	
+	static byte[] getPasswordHash(String user, String password) {
+		byte[] salt = getSalt(user);
+		byte[] hash = new byte[0];
+		for (int i=0; i<STRETCH_COUNT; i++) {
+			hash = ArrayUtils.addAll(hash, password.getBytes());
+			hash = ArrayUtils.addAll(hash, salt);
+			hash = DigestUtils.sha256(hash);
+		}
+		return hash;
+	}
 	
 	private boolean tryAuth(String authHeader, HttpServletRequest req) {
 		if (authHeader == null) return false;
 		String[] pair = authHeader.split(" ");
 		if (pair.length != 2) {
-			logger.severe("faild to parse authHeader" + authHeader);
 			return false;
 		}
-		if (!pair[0].equals("Basic")) {
-			logger.severe("unsupported login scheme: "+pair[0]);
-		}
-		logger.severe("pair[1]="+pair[1]);
 		String decoded= new String(Base64.decodeBase64(pair[1].getBytes()));
 		String[] userPass = decoded.split(":");
-		logger.severe("userPass[0]="+userPass[0]+", userPass[1]="+userPass[1]);
-		Set<String> keys = userMap.keySet();
-		for (String k: keys) {
-			logger.severe("key="+k+", value="+userMap.get(k));
+		byte[] pass = null;
+		try {
+			pass = Hex.decodeHex(userMap.get(userPass[0]).toCharArray());
+		} catch (Exception e) {
+ 			e.printStackTrace();
 		}
+		byte[] hash = getPasswordHash(userPass[0], userPass[1]);
 		if (userPass.length != 2 || !userMap.containsKey(userPass[0])
-				|| !userMap.get(userPass[0]).equals(userPass[1])) {
-			logger.severe("AuthFailed: " + decoded);
+			|| !ArrayUtils.isEquals(pass, hash)) {
 			return false;
 		}
-		logger.info("authentication success for user '" + userPass[0] + "'");
 		req.setAttribute(BASIC_AUTH_USERNAME, userPass[0]);
 		return true;
 	}
