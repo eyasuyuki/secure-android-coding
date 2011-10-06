65c35dd220abd1e8974d66de702b8170056716f1
eyasuyuki@gmail.com <eyasuyuki@gmail.com@99484277-7f74-ce71-cf82-0c6185048e4c>
2011-10-06 21:25:18 +0000

    add getAuthToken

--- trunk/AccountManagerClient/src/com/example/account/manager/client/Main.java
+++ trunk/AccountManagerClient/src/com/example/account/manager/client/Main.java
@@ -1,29 +1,83 @@
 package com.example.account.manager.client;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class Main extends ListActivity {
-    /** Called when the activity is first created. */
+	private static final String TAG = Main.class.getName();
+	
+	public static final int GET_ACCOUNT = 1;
+	public static final String ACCOUNT_TYPE = "com.google";
+	public static final String APP_ENGINE = "ah";
+    public static final String AUTH_TOKEN_KEY = "authtoken";
+
+	Handler handler = new Handler();
+
 	Account account = null;
-    @Override
+	String authToken = null;
+	
+	
+	final class NutrimancerCallback implements AccountManagerCallback<Bundle> {
+		@Override
+		public void run(AccountManagerFuture<Bundle> future) {
+			try {
+				Bundle result = future.getResult();
+				if (result.containsKey(AccountManager.KEY_INTENT)) {
+				        Intent intent =
+				                (Intent) result.get(AccountManager.KEY_INTENT);
+				        Main.this.startActivityForResult(intent, Main.GET_ACCOUNT);
+				        return;
+				}
+				
+				authToken =
+				        result.getString(AccountManager.KEY_AUTHTOKEN);
+				Log.d(TAG, "getAuthToken: authToken="+authToken);
+			} catch (OperationCanceledException e) {
+				e.printStackTrace();
+			} catch (AuthenticatorException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} 
+	}
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         checkAccount();
         initAccount();
+        
+        // login by account manager
+        AccountManager am = AccountManager.get(this);
+        AccountManagerFuture<Bundle> future =
+        	am.getAuthToken(account, APP_ENGINE, true, new NutrimancerCallback(), handler);
+        try {
+			authToken = future.getResult().getString(AUTH_TOKEN_KEY);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        Log.d(TAG, "onCreate: authToken="+authToken);
     }
     
     void checkAccount() {
@@ -46,7 +100,7 @@ public class Main extends ListActivity {
     
     void initAccount() {
         Account[] accounts =
-        	AccountManager.get(this).getAccountsByType("com.google");
+        	AccountManager.get(this).getAccountsByType(ACCOUNT_TYPE);
         Map<String, Account> ahash = new HashMap<String, Account>();
         for (Account a: accounts) {
         	ahash.put(a.name, a);
@@ -79,4 +133,17 @@ public class Main extends ListActivity {
 		}
 		return true;
 	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            //Log.d(TAG, "requestCode="+requestCode+", resultCode="+resultCode+", data"+data);
+            switch (requestCode) {
+            case GET_ACCOUNT:
+                    if (resultCode == RESULT_OK && data != null) {
+                            authToken = data.getStringExtra(AUTH_TOKEN_KEY);
+                            Log.d(TAG, "onActivityResult: authToken="+authToken);
+                    }
+                    break;
+            }
+            super.onActivityResult(requestCode, resultCode, data);
+    }
 }
