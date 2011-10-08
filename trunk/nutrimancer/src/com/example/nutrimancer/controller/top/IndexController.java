1ee1d7f009d4e3fb1be4ee6beeb37b80959a9e49
eyasuyuki@gmail.com <eyasuyuki@gmail.com@99484277-7f74-ce71-cf82-0c6185048e4c>
2011-10-08 05:52:18 +0000

    add email add logoutUrl

--- trunk/nutrimancer/src/com/example/nutrimancer/controller/top/IndexController.java
+++ trunk/nutrimancer/src/com/example/nutrimancer/controller/top/IndexController.java
@@ -7,15 +7,23 @@ import org.slim3.controller.Navigation;
 
 import com.example.nutrimancer.model.FoodLog;
 import com.example.nutrimancer.service.EntryService;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 public class IndexController extends Controller {
 
     EntryService service = new EntryService();
-    
+
     @Override
     public Navigation run() throws Exception {
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+        String thisURL = request.getRequestURI();
         List<FoodLog> foodLogList = service.getFoodLogList();
         requestScope("foodLogList", foodLogList);
+        requestScope("email", user.getEmail());
+        requestScope("logoutUrl", userService.createLogoutURL(thisURL));
         return forward("index.jsp");
     }
 }
