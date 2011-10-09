7c7b37d5b371d1775cad6b7f09fc6ef17412f961
eyasuyuki@gmail.com <eyasuyuki@gmail.com@99484277-7f74-ce71-cf82-0c6185048e4c>
2011-10-09 03:32:21 +0000

    add json constructor

--- AccountManagerClient/src/com/example/account/manager/client/provider/FoodLog.java
+++ AccountManagerClient/src/com/example/account/manager/client/provider/FoodLog.java
@@ -1,23 +1,89 @@
 package com.example.account.manager.client.provider;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class FoodLog {
-	public FoodLog() {}
-	private int id = 0;
-	private String key = null;
-    private long version = 0L;
-    private String user = null;
-    private String logDate = null;
-    private String time = null;
-    private String food = null;
-    private double kcal = 0.0;
-    private double salt = 0.0;
+	public static final SimpleDateFormat DF =
+		new SimpleDateFormat("yyyy-MM-dd");
+	public static final SimpleDateFormat DTF =
+		new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+	public static final String KEY_KEY         = "key";
+	public static final String VERSION_KEY     = "version";
+	public static final String USER_KEY        = "user";
+	public static final String LOG_DATE_KEY    = "logDate";
+	public static final String TIME_KEY        = "time";
+	public static final String FOOD_KEY        = "food";
+	public static final String KCAL_KEY        = "kcal";
+	public static final String SALT_KEY        = "salt";
+	public static final String CREATE_DATE_KEY = "createDate";
+	private long id           = 0L;
+	private String key        = null;
+    private long version      = 0L;
+    private String user       = null;
+    private String logDate    = null;
+    private String time       = null;
+    private String food       = null;
+    private double kcal       = 0.0;
+    private double salt       = 0.0;
     private String createDate = null;
-	public int getId() {
+	public FoodLog() {}
+	public FoodLog(JSONObject json) {
+		try {
+			key = json.getString(KEY_KEY);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		try {
+			version = json.getLong(VERSION_KEY);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		try {
+			user = json.getString(USER_KEY);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		try {
+			long ltime = json.getLong(LOG_DATE_KEY);
+			logDate = DF.format(new Date(ltime));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		try {
+			time = json.getString(TIME_KEY);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		try {
+			food = json.getString(FOOD_KEY);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		try {
+			kcal = json.getDouble(KCAL_KEY);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		try {
+			salt = json.getDouble(SALT_KEY);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		try {
+			long ctime = json.getLong(CREATE_DATE_KEY);
+			createDate = DTF.format(new Date(ctime));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+	public long getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 	public String getKey() {
