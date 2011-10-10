6f40941bccd532b31a77c9b050445bce37c83751
eyasuyuki@gmail.com <eyasuyuki@gmail.com@99484277-7f74-ce71-cf82-0c6185048e4c>
2011-10-10 11:40:06 +0000

    remove over CR.

--- trunk/SelectAccount/src/com/example/select/account/SelectAccount.java
+++ trunk/SelectAccount/src/com/example/select/account/SelectAccount.java
@@ -24,7 +24,6 @@ public class SelectAccount extends ListActivity {
         setContentView(R.layout.main);
 		AccountManager am = AccountManager.get(this);
 
-
 		accounts = am.getAccountsByType("com.google");
 		if (accounts == null || accounts.length <= 0) {
 			addAccount();
