e7e0614d7060de54644941ebac361d4bf3f8c399
eyasuyuki@gmail.com <eyasuyuki@gmail.com@99484277-7f74-ce71-cf82-0c6185048e4c>
2011-10-16 12:42:45 +0000

    fix indent

--- AccountManagerClient/src/com/example/account/manager/client/Main.java
+++ AccountManagerClient/src/com/example/account/manager/client/Main.java
@@ -26,194 +26,178 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class Main extends ListActivity {
-	private static final String TAG = Main.class.getName();
-	
-	public static final int ACCOUNT_SELECTED = 1;
-	public static final int GRANT_AUTH_TOKEN = 2;
-	public static final String ACCOUNT_TYPE = "com.google";
-	Handler handler = new Handler();
-	DefaultHttpClient client = null;;
+    private static final String TAG = Main.class.getName();
 
-	Account account = null;
-	String authToken = null;
-	
-	final class InvalidateTokenListener implements TokenListener {
-		@Override
-		public void invalidate() {
-			AccountManager am = AccountManager.get(Main.this);
-			am.invalidateAuthToken(AsyncAuthToken.APP_ENGINE, authToken); // dispose token
-			authToken = null;
-			handler.post(new Runnable() {
-				@Override
-				public void run() {
-					Toast.makeText(
-							Main.this,
-							Main.this.getString(R.string.invalidate_token_message),
-							Toast.LENGTH_SHORT).show();
-				}
-			});
-		}
-	}
-	
-	final class NutrimancerCallback implements AccountManagerCallback<Bundle> {
-		private final String TAG = NutrimancerCallback.class.getName();
-		@Override
-		public void run(AccountManagerFuture<Bundle> future) {
-			try {
-				Bundle result = future.getResult();
-				if (result.containsKey(AccountManager.KEY_INTENT)) {
-				        Intent intent =
-				                (Intent) result.get(AccountManager.KEY_INTENT);
-				        Main.this.startActivityForResult(intent, Main.GRANT_AUTH_TOKEN);
-				        return;
-				}
-				
-				authToken =
-				        result.getString(AccountManager.KEY_AUTHTOKEN);
-				Log.d(TAG, "run: authToken="+authToken);
-				if (authToken != null) {
-					client = new DefaultHttpClient();
-					AsyncUpdate update =
-						new AsyncUpdate(
-								Main.this,
-								client,
-								getListView());
-					AsyncCookie async =
-						new AsyncCookie(
-								Main.this,
-								client,
-								new InvalidateTokenListener(),
-								update);
-					async.execute(authToken);
-				}
-			} catch (OperationCanceledException e) {
-				e.printStackTrace();
-			} catch (AuthenticatorException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} 
-	}
+    public static final int ACCOUNT_SELECTED = 1;
+    public static final int GRANT_AUTH_TOKEN = 2;
+    public static final String ACCOUNT_TYPE = "com.google";
+    Handler handler = new Handler();
+    DefaultHttpClient client = null;;
 
-	@Override
+    Account account = null;
+    String authToken = null;
+
+    final class InvalidateTokenListener implements TokenListener {
+        @Override
+        public void invalidate() {
+            AccountManager am = AccountManager.get(Main.this);
+            am.invalidateAuthToken(AsyncAuthToken.APP_ENGINE, authToken); // dispose
+                                                                          // token
+            authToken = null;
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(
+                            Main.this,
+                            Main.this
+                                    .getString(R.string.invalidate_token_message),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    final class NutrimancerCallback implements AccountManagerCallback<Bundle> {
+        private final String TAG = NutrimancerCallback.class.getName();
+
+        @Override
+        public void run(AccountManagerFuture<Bundle> future) {
+            try {
+                Bundle result = future.getResult();
+                if (result.containsKey(AccountManager.KEY_INTENT)) {
+                    Intent intent = (Intent) result
+                            .get(AccountManager.KEY_INTENT);
+                    Main.this.startActivityForResult(intent,
+                            Main.GRANT_AUTH_TOKEN);
+                    return;
+                }
+
+                authToken = result.getString(AccountManager.KEY_AUTHTOKEN);
+                Log.d(TAG, "run: authToken=" + authToken);
+                if (authToken != null) {
+                    client = new DefaultHttpClient();
+                    AsyncUpdate update = new AsyncUpdate(Main.this, client,
+                            getListView());
+                    AsyncCookie async = new AsyncCookie(Main.this, client,
+                            new InvalidateTokenListener(), update);
+                    async.execute(authToken);
+                }
+            } catch (OperationCanceledException e) {
+                e.printStackTrace();
+            } catch (AuthenticatorException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         checkAccount();
         initAccount();
 
         if (account != null) {
-        	asyncAuthToken();
+            asyncAuthToken();
         }
     }
-    
-    void checkAccount() {
-    	SharedPreferences sp =
-    		PreferenceManager.getDefaultSharedPreferences(this);
-    	String key = getString(R.string.default_account_key);
-    	if (!sp.contains(key)) {
-    		startSelectAccount();
-    	}
-    }
-    
-    void startSelectAccount() {
-		Intent intent = new Intent();
-		intent.setClass(this, SelectAccount.class);
-		startActivityForResult(intent, ACCOUNT_SELECTED);
-    }
-    
-    void initAccount() {
-        Account[] accounts =
-        	AccountManager.get(this).getAccountsByType(ACCOUNT_TYPE);
-        Map<String, Account> ahash = new HashMap<String, Account>();
-        for (Account a: accounts) {
-        	ahash.put(a.name, a);
-        }
-    	
-        SharedPreferences sp =
-    		PreferenceManager.getDefaultSharedPreferences(this);
-    	
-        String key = getString(R.string.default_account_key);
-    	String ac = sp.getString(key, null);
-    	if (ac != null) {
-    		account = ahash.get(ac);
-    	}
-    }
-    
-    void asyncAuthToken() {
-		client = new DefaultHttpClient();
-		AsyncUpdate update =
-			new AsyncUpdate(
-					this,
-					client,
-					getListView());
-		AsyncCookie cookie =
-			new AsyncCookie(
-					this,
-					client,
-					new InvalidateTokenListener(),
-					update);
-		AsyncAuthToken async =
-			new AsyncAuthToken(
-					this,
-					handler,
-					account,
-					new NutrimancerCallback(),
-					cookie
-					);
-		async.execute();
-    }
-    
-	@Override
-	protected void onResume() {
-		super.onResume();
-		asyncAuthToken();
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		MenuInflater inflater = this.getMenuInflater();
+    void checkAccount() {
+        SharedPreferences sp = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        String key = getString(R.string.default_account_key);
+        if (!sp.contains(key)) {
+            startSelectAccount();
+        }
+    }
+
+    void startSelectAccount() {
+        Intent intent = new Intent();
+        intent.setClass(this, SelectAccount.class);
+        startActivityForResult(intent, ACCOUNT_SELECTED);
+    }
+
+    void initAccount() {
+        Account[] accounts = AccountManager.get(this).getAccountsByType(
+                ACCOUNT_TYPE);
+        Map<String, Account> ahash = new HashMap<String, Account>();
+        for (Account a : accounts) {
+            ahash.put(a.name, a);
+        }
+
+        SharedPreferences sp = PreferenceManager
+                .getDefaultSharedPreferences(this);
+
+        String key = getString(R.string.default_account_key);
+        String ac = sp.getString(key, null);
+        if (ac != null) {
+            account = ahash.get(ac);
+        }
+    }
+
+    void asyncAuthToken() {
+        client = new DefaultHttpClient();
+        AsyncUpdate update = new AsyncUpdate(this, client, getListView());
+        AsyncCookie cookie = new AsyncCookie(this, client,
+                new InvalidateTokenListener(), update);
+        AsyncAuthToken async = new AsyncAuthToken(this, handler, account,
+                new NutrimancerCallback(), cookie);
+        async.execute();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        asyncAuthToken();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = this.getMenuInflater();
         inflater.inflate(R.menu.options, menu);
         return true;
-	}
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.menu_add_food_log:
-			Intent intent = new Intent();
-			intent.setClass(this, AddFoodLog.class);
-			intent.putExtra(AccountManager.KEY_AUTHTOKEN, authToken);
-			startActivity(intent);
-			break;
-		case R.id.menu_select_account:
-			startSelectAccount();
-			break;
-		}
-		return true;
-	}
-	
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_add_food_log:
+            Intent intent = new Intent();
+            intent.setClass(this, AddFoodLog.class);
+            intent.putExtra(AccountManager.KEY_AUTHTOKEN, authToken);
+            startActivity(intent);
+            break;
+        case R.id.menu_select_account:
+            startSelectAccount();
+            break;
+        }
+        return true;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            Log.d(TAG, "onActivityResult: requestCode="+requestCode+", resultCode="+resultCode+", data="+data);
-            switch (requestCode) {
-            case ACCOUNT_SELECTED:
-            	if (resultCode == RESULT_OK && data != null) {
-            		account =
-            			(Account)data.getExtras().get(SelectAccount.DEFAULT_ACCOUNT_KEY);
-            		Log.d(TAG, "onActivityResult: account="+account);
-            		asyncAuthToken();
-            	}
-            case GRANT_AUTH_TOKEN:
-                    if (resultCode == RESULT_OK && authToken == null) {
-                    	asyncAuthToken();
-                    	Log.d(TAG, "onActivityResult: GRAND_AUTH_TOKEN: authToken="+authToken);
-                    }
-                    break;
+        Log.d(TAG, "onActivityResult: requestCode=" + requestCode
+                + ", resultCode=" + resultCode + ", data=" + data);
+        switch (requestCode) {
+        case ACCOUNT_SELECTED:
+            if (resultCode == RESULT_OK && data != null) {
+                account = (Account) data.getExtras().get(
+                        SelectAccount.DEFAULT_ACCOUNT_KEY);
+                Log.d(TAG, "onActivityResult: account=" + account);
+                asyncAuthToken();
             }
-            super.onActivityResult(requestCode, resultCode, data);
+        case GRANT_AUTH_TOKEN:
+            if (resultCode == RESULT_OK && authToken == null) {
+                asyncAuthToken();
+                Log.d(TAG, "onActivityResult: GRAND_AUTH_TOKEN: authToken="
+                        + authToken);
+            }
+            break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
